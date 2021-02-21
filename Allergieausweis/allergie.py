e01582b8a59bc64fccfcd0554403f790ef55db63
Thilo Schmid <thiloschmid@bluewin.ch>
2021-02-21 15:54:24 +0100

    made template dynamic

--- Allergieausweis/allergie.py
+++ Allergieausweis/allergie.py
@@ -8,88 +8,99 @@ Created on Fri Feb 19 19:44:23 2021
 import csv
 import random
 
-#Name
+import os
+
+path = os.path.dirname(__file__)
+
+# Name
 
 vocals = 'aeiou'
 
 cons = 'bcdfghjklmnpqrstvwxyz'
+
 
 def fname():
     w1 = random.choice(cons)
     w2 = w1 + random.choice(vocals)
     w3 = w2 + random.choice(cons)
     w4 = w3 + random.choice(vocals)
-    w5 = w4 + random.choice(cons)  
+    w5 = w4 + random.choice(cons)
     return w5
 
 
-#Geburtstag
+# Geburtstag
 
-numbers = list(range(1,29))
-months = ['Januar','Februar','Marz','April','Mai','Juni','Juli','August','September','Oktober','November','Dezember']
+numbers = list(range(1, 29))
+months = ['Januar', 'Februar', 'Marz', 'April', 'Mai', 'Juni',
+          'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember']
+
 
 def fdate():
-    geburi = str(random.choice(numbers)) + '.' + random.choice(months)
+    geburi = str(random.choice(numbers)) + '. ' + random.choice(months)
     return geburi
 
 
-#Medikamenteneinnahme
+# Medikamenteneinnahme
 
 #ljn = [['ja'], ['nein']]
 
 def fmedi():
-    jn = random.choice([0,1])
+    jn = random.choice([0, 1])
     return jn
 
 
-#Blutgruppe
+# Blutgruppe
 
-lbg = [['A'], ['B'], ['AB'], ['0']]
+lbg = ['A', 'B', 'AB', '0']
+
 
 def fblut():
     bg = random.choice(lbg)
     return bg
-    
-#list of allergies
+
+# list of allergies
+
 
 def fallergien():
-    with open ('allergien.csv', newline='') as g:
+    with open("/".join([path, 'allergien.csv']), newline='') as g:
         reader1 = csv.reader(g)
         lallergies = list(reader1)
-    nallergies = random.choice([1,2,2,2,3,3])
+    nallergies = random.choice([1, 2, 2, 2, 3, 3])
     allergies = []
-    for i in range(0,nallergies):
-        allergies = allergies + random.choice(lallergies)    
+    for i in range(0, nallergies):
+        allergies = allergies + random.choice(lallergies)
     return allergies
-        
-#Ausweisnummer
+
+# Ausweisnummer
+
 
 def fnummer():
-    numb = random.randint(10000,99999)
+    numb = random.randint(10000, 99999)
     return numb
 
 
-#Ausstellungsland
+# Ausstellungsland
 
 def fland():
-    with open ('staaten.csv', newline='') as f:
+    with open("/".join([path, 'staaten.csv']), newline='') as f:
         reader = csv.reader(f)
-        lcountries = list(reader)      
+        print(reader)
+        lcountries = list(reader)
     aland = random.choice(lcountries)
-    return aland    
+    return aland
 
 
-#Generiere Ausweise
+# Generiere Ausweise
 
-#4, 6 oder 8 Ausweise
+# 4, 6 oder 8 Ausweise
 
 nausw = 4
 #nausw = 6
 #nausw = 8
 
-#XXX wie daten für ausweis speichern, wie allergien (versch. längen)?
+# XXX wie daten für ausweis speichern, wie allergien (versch. längen)?
 
-#Fragen
+# Fragen
 """
 #Name, Geburi, Medi, BG, Allergien, Ausweisnr, Land
 def ausweise(nausw):
@@ -102,33 +113,28 @@ def ausweise(nausw):
     
 """
 
-names, geburis, medis, bluts, nummers = ausweise(nausw)
+# names, geburis, medis, bluts, nummers = ausweise(nausw)
+
 
 def create_person():
-    person = {'name':fname(),'geburi':fdate(),'medis': fmedi(),
-              'bg':fblut(),'nummer':fnummer(),'allergien':fallergien(),'land':fland()}
+    person = {'name': fname(), 'geburi': fdate(), 'medis': fmedi(),
+              'bg': fblut(), 'nummer': fnummer(), 'allergien': fallergien(), 'land': fland()}
     return person
+
 
 def create_uswiise(nausw):
     return [create_person() for i in range(nausw)]
-    
-
-#Eine Antwort richtig
-
-n1 = random.choice(nummers)
-print('In welchem Land wurde der Allergieausweis mit der Ausweisnummer '+str(n1)+' ausgestellt?')
 
 
 
-#keine Antwort richtig
+# Eine Antwort richtig
 
-rbg = random.choice(lbg)
-print('Welche Person nimmt Medikamente ein und hat zudem die Blutgruppe'+str(rbg)+'?')
-
-
-    
+# n1 = random.choice(nummers)
+# print('In welchem Land wurde der Allergieausweis mit der Ausweisnummer ' +
+#       str(n1)+' ausgestellt?')
 
 
+# keine Antwort richtig
 
-
-
+# rbg = random.choice(lbg)
+# print('Welche Person nimmt Medikamente ein und hat zudem die Blutgruppe'+str(rbg)+'?')
--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from flask import Flask, url_for, render_template
 
+from Allergieausweis import allergie
+
 app = Flask(__name__)
 
-
+persons = allergie.create_uswiise(4)
 
 @app.route('/')
 def hello_world():
-    return render_template('main.html')
+    return render_template('main.html', person=persons[0])
 
 if __name__ == '__main__':
     app.run(threaded=True, port=5000)
